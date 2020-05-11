8bb1173ba18b704df234223b8b75273d48e2d6c1
leaf222 <ahtlye@163.com>
2020-05-11 23:34:58 +0800

    Triangle_Boundary

---
+++ Test/Test/Forms/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.TestName = new System.Windows.Forms.ComboBox();
+            this.TestMethod = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.StartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TestName
+            // 
+            this.TestName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TestName.FormattingEnabled = true;
+            this.TestName.Location = new System.Drawing.Point(492, 85);
+            this.TestName.Name = "TestName";
+            this.TestName.Size = new System.Drawing.Size(155, 23);
+            this.TestName.TabIndex = 0;
+            this.TestName.SelectedIndexChanged += new System.EventHandler(this.TestName_SelectedIndexChanged);
+            // 
+            // TestMethod
+            // 
+            this.TestMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TestMethod.FormattingEnabled = true;
+            this.TestMethod.Location = new System.Drawing.Point(492, 137);
+            this.TestMethod.Name = "TestMethod";
+            this.TestMethod.Size = new System.Drawing.Size(155, 23);
+            this.TestMethod.TabIndex = 1;
+            this.TestMethod.SelectedIndexChanged += new System.EventHandler(this.TestMethod_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(92, 85);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(492, 189);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(112, 23);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "StartTest";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.TestMethod);
+            this.Controls.Add(this.TestName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox TestName;
+        private System.Windows.Forms.ComboBox TestMethod;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ Test/Test/Forms/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //为两个下拉框添加内容
+        private void ComboBoxLoad()
+        {
+            TestName.Items.Add("万年历");
+            TestName.Items.Add("三角形");
+            TestMethod.Items.Add("边界值");
+            TestMethod.Items.Add("等价类");
+        }
+
+        private void TestName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TestMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBoxLoad();
+        }
+    }
+}
