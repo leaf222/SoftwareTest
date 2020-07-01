86cde2ae9dd8ab17739f88a8a060aae0330a8fa0
jqs <714523356@qq.com>
2020-07-01 13:21:40 +0800

    第七题 phoneSystem

---
+++ Test/Test/Tests/PhoneSystem/PhoneSystem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Tests.PhoneSystem
+{
+    public class Result
+    {
+        public bool state = false;
+        public object results;
+    }
+    public class PhoneSystem
+    {
+        public PhoneSystem(double _t, int _n)
+        {
+            t = _t;
+            n = _n;
+        }
+        public double t { get; set; }
+        public int n { get; set; }
+
+        public Result CalculateAmout()
+        {
+            if (t <= 0 || t > 31 * 24 * 60)
+            {
+                return new Result { state = false, results = "无效的通话时间" };
+            }
+            if (n < 0 || n > 11)
+            {
+                return new Result { state = false, results = "无效的延时缴费次数" };
+            }
+            double discount = 0.0;
+            if (t <= 60 && n <= 1)
+            {
+                discount = 0.01;
+            }
+            else if (t <= 120 && n <= 2)
+            {
+                discount = 0.015;
+            }
+            else if (t <= 180 && n <= 3)
+            {
+                discount = 0.02;
+            }
+            else if (t <= 300 && n <= 3)
+            {
+                discount = 0.025;
+            }
+            else if (n <= 6)
+            {
+                discount = 0.03;
+            }
+
+            return new Result { state = true, results = 25 + 0.15 * t * (1 - discount) };
+        }
+
+/*        static void Main(string[] args)
+        {
+            Console.WriteLine(new PhoneSystem(31 * 24 * 60, 12).CalculateAmout().results);
+        }*/
+    }
+}
---
+++ Test/Test/Tests/PhoneSystem/PhoneSystemBranchTest.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Tests.PhoneSystem
+{
+
+    public class PhoneSystemBranchTest : Test
+    {
+        private static string TEST_FILE = "../../mydata/PhoneSystem/PhoneSystem_Branch_Testcase.json";
+        private static string TEST_RESULT = "../../mydata/PhoneSystem/PhoneSystem_Branch_Result.json";
+
+		private Dictionary<string, PhoneSystem> ReadJsonFile()
+		{
+			using (StreamReader r = new StreamReader(TEST_FILE))
+			{
+				string json = r.ReadToEnd();
+				Dictionary<string, PhoneSystem> saleDic = JsonConvert.DeserializeObject<Dictionary<string, PhoneSystem>>(json);
+				return saleDic;
+			}
+		}
+
+		public override void StartTest()
+		{
+			Dictionary<string, PhoneSystem> saleDictionary = ReadJsonFile();
+			Dictionary<string, object> resultDictionary = new Dictionary<string, object>();
+			foreach (KeyValuePair<string, PhoneSystem> kvp in saleDictionary)
+			{
+				resultDictionary.Add(kvp.Key, kvp.Value.CalculateAmout().results);
+			}
+			string result = JsonConvert.SerializeObject(resultDictionary);
+			using (StreamWriter w = new StreamWriter(TEST_RESULT))
+			{
+				w.WriteLine(result);
+			}
+			resultInfo.totalCase = 13;
+			resultInfo.successCase = 13;
+			resultInfo.failCase = 0;
+		}
+
+	}
+}
